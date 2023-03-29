2a0476871c35d344364c39b51571f56cf06273db
Italo Ferreira <grd788@gmail.com>
2023-03-28 23:52:15 -0500

    feat :sparkles: home and paymentbuttons for purchase components

---
+++ src/components/Home.tsx
@@ -0,0 +1,71 @@
+import { Coffee, Package, Timer } from '@phosphor-icons/react'
+import { Card } from './Card'
+import Ilustration from '../assets/illustation.png'
+import CoffeJSON from '../../CoffesJSON.json'
+
+export function Home() {
+  return (
+    <>
+      <div className="relative mx-auto z-10 py-24 grid grid-cols-2 text-base-800">
+        <main className="max-w-[588px]">
+          <h1 className="text-5xl font-extrabold leading-[1.3]">
+            Encontre o café perfeito para qualquer hora do dia
+          </h1>
+          <p className="mt-4 text-xl">
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+          <div className="bg-app absolute inset-0" />
+
+          <ul className="mt-[70px] grid grid-cols-2 gap-y-5">
+            <li className="flex items-center gap-3">
+              <div className="w-8 h-8 p-2 bg-yellow-500 rounded-full">
+                <Timer color="#FFFFFFFF" size={16} />
+              </div>
+              Compra simples e segura
+            </li>
+            <li className="flex items-center gap-3">
+              <div className="w-8 h-8 p-2 bg-base-700 rounded-full">
+                <Package color="#FFFFFFFF" size={16} />
+              </div>
+              Embalagem mantém o café intacto
+            </li>
+            <li className="flex items-center gap-3">
+              <div className="w-8 h-8 p-2 bg-yellow-300 rounded-full">
+                <Timer color="#FFFFFFFF" size={16} />
+              </div>
+              Entrega rápida e rastreada
+            </li>
+            <li className="flex items-center gap-3">
+              <div className="w-8 h-8 p-2 bg-purple-300 rounded-full">
+                <Coffee color="#FFFFFFFF" size={16} />
+              </div>
+              O café chega fresquinho até você
+            </li>
+          </ul>
+        </main>
+
+        <div className="flex items-center justify-center">
+          <img src={Ilustration} alt="" />
+        </div>
+      </div>
+
+      <h2 className="text-[32px] font-extrabold mb-12">Nossos cafés</h2>
+      <ul className=" flex flex-wrap gap-11 mb-36">
+        {CoffeJSON.map(coffe => {
+          return (
+            <Card
+              key={coffe.id}
+              id={coffe.id}
+              image={coffe.image}
+              flags={coffe.tags}
+              name={coffe.title}
+              description={coffe.description}
+              price={coffe.priceInCents}
+            />
+          )
+        })}
+      </ul>
+    </>
+  )
+}
---
+++ src/components/PaymentButtons.tsx
@@ -0,0 +1,48 @@
+import { Bank, CreditCard, Money } from '@phosphor-icons/react'
+import { useState } from 'react'
+
+export function PaymentButtons() {
+  const [selectedButton, setSelectedButton] = useState('')
+
+  const handleButtonClick = (type: string) => {
+    setSelectedButton(type)
+  }
+
+  return (
+    <div className="flex justify-center space-x-4">
+      <button
+        onClick={() => handleButtonClick('credit')}
+        className={`${
+          selectedButton === 'credit'
+            ? 'bg-purple-100 border-[1px] border-purple-300'
+            : 'bg-base-400 text-gray-700'
+        } w-[178px] h-[51px] flex items-center text-xs uppercase gap-3 py-2 px-4 rounded-md`}
+      >
+        <CreditCard color="#8047F8" size={16} />
+        Cartão de Crédito
+      </button>
+      <button
+        onClick={() => handleButtonClick('debit')}
+        className={`${
+          selectedButton === 'debit'
+            ? 'bg-purple-100 border-[1px] border-purple-300'
+            : 'bg-base-400 text-gray-700'
+        } w-[178px] h-[51px] flex items-center text-xs uppercase gap-3 py-2 px-4 rounded-md`}
+      >
+        <Bank color="#8047F8" size={16} />
+        Cartão de Débito
+      </button>
+      <button
+        onClick={() => handleButtonClick('cash')}
+        className={`${
+          selectedButton === 'cash'
+            ? 'bg-purple-100 border-[1px] border-purple-300'
+            : 'bg-base-400 text-gray-700'
+        } w-[178px] h-[51px] flex items-center text-xs uppercase gap-3 py-2 px-4 rounded-md`}
+      >
+        <Money color="#8047F8" size={16} />
+        Dinheiro
+      </button>
+    </div>
+  )
+}
