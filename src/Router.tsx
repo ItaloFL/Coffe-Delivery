df85d50dc7fc3e03a2f253e4f1038aed4df59e45
Italo Ferreira <grd788@gmail.com>
2023-04-04 23:48:19 -0500

    refactor :recycle: router component render

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import { Checkout } from './components/Cheout'
 import { Home } from './components/Home'
 import { DefaultLayout } from './layout/DefaultLayout'
+// import { ShoppingCart } from './components/ShoppingCart'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/cart" element={<Checkout />} />
       </Route>
     </Routes>
   )
