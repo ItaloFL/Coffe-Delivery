db44cdfc0344334070048f2f8408b368fe03e383
Italo Ferreira <grd788@gmail.com>
2023-04-10 22:59:21 -0500

    refactor :recyle: input to type radio and clearCart function

--- src/components/Cheout.tsx
+++ src/components/Cheout.tsx
@@ -1,64 +1,226 @@
 import { CurrencyDollar, MapPinLine } from '@phosphor-icons/react'
-import { Cart } from './Cart'
 import { PaymentButtons } from './PaymentButtons'
+import { SelectedCoffes } from './SelectedCoffes'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext, useState } from 'react'
+import { CoffeContext } from '../context/CoffeContext'
+import { useNavigate } from 'react-router-dom'
+import { FormProvider } from 'react-hook-form'
+
+export const ClientAdressData = zod.object({
+  cep: zod
+    .string()
+    .nonempty('CEP obrigatório')
+    .length(8, 'Insira um cep válido.'),
+  rua: zod
+    .string()
+    .nonempty('Rua obrigatório')
+    .min(2, 'Insira uma rua válida')
+    .transform(roadName => {
+      return roadName
+        .trim()
+        .split(' ')
+        .map(word => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1))
+        })
+        .join(' ')
+    }),
+  numero: zod
+    .string()
+    .nonempty('Número de residencia obrigatório')
+    .transform(numero => Number(numero)),
+  complemento: zod.string().optional(),
+  uf: zod
+    .string()
+    .nonempty('Insira a UF')
+    .length(2, 'Insira uma UF válida')
+    .transform(name => name.toLocaleUpperCase()),
+  bairro: zod
+    .string()
+    .nonempty('Insira o seu bairro')
+    .transform(districtName => {
+      return districtName
+        .trim()
+        .split(' ')
+        .map(word => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1))
+        })
+        .join(' ')
+    }),
+  cidade: zod
+    .string()
+    .nonempty('Insira a sua cidade')
+    .transform(cityName => {
+      return cityName
+        .trim()
+        .split(' ')
+        .map(word => {
+          return word[0].toLocaleUpperCase().concat(word.substring(1))
+        })
+        .join(' ')
+    }),
+  paymentMethod: zod.string({
+    errorMap: () => {
+      return { message: 'Informe o método de pagamento' }
+    }
+  })
+})
+
+export type clientAdressData = zod.infer<typeof ClientAdressData>
 
 export function Checkout() {
+  const { setAdressForSucessData, clearCart } = useContext(CoffeContext)
+  const navigate = useNavigate()
+
+  const orderForm = useForm<clientAdressData>({
+    resolver: zodResolver(ClientAdressData)
+  })
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = orderForm
+
+  console.log(errors)
+
+  function handleSubmitPurchase(data: clientAdressData) {
+    console.log(data)
+    setAdressForSucessData(data)
+    navigate('/sucess')
+    clearCart()
+  }
+
   return (
-    <div className="grid grid-cols-checkout gap-8 mt-36">
-      <div>
-        <h2 className="font-bold text-lg">Complete seu pedido</h2>
+    <FormProvider {...orderForm}>
+      <form
+        className="grid grid-cols-checkout gap-8 mt-36"
+        onSubmit={handleSubmit(handleSubmitPurchase)}
+      >
         <div>
-          <div className="bg-base-200 mt-4 p-10">
-            <div className="mb-8">
-              <div className="flex items-center gap-2">
-                <MapPinLine color="#C47F17" size={22} />
-                <h4 className="text-base-800">Endereço de entrega</h4>
+          <h2 className="font-bold text-lg">Complete seu pedido</h2>
+          <div>
+            <div className="bg-base-200 mt-4 p-10">
+              <div className="mb-8">
+                <div className="flex items-center gap-2">
+                  <MapPinLine color="#C47F17" size={22} />
+                  <h4 className="text-base-800">Endereço de entrega</h4>
+                </div>
+                <p className="text-sm ml-[30px] text-base-700">
+                  Informe o endereço onde deseja receber seu pedido
+                </p>
               </div>
-              <p className="text-sm ml-[30px] text-base-700">
-                Informe o endereço onde deseja receber seu pedido
-              </p>
-            </div>
-            <form
-              action=""
-              id="formPayment"
-              className="flex flex-col gap-y-4 placeholder-base-600 placeholder:text-sm"
-            >
-              <input type="text" placeholder="CEP" className="w-[200px]" />
-              <input type="text" placeholder="Rua" />
-              <div className="grid grid-cols-inputGrid gap-3">
-                <input type="text" placeholder="Número" />
-                <input type="text" placeholder="Complemento" />
-              </div>
-              <div className="grid grid-cols-secondInputGrid gap-3">
-                <input type="text" placeholder="Bairro" />
-                <input type="text" placeholder="Cidade" />
-                <input type="text" placeholder="UF" />
-              </div>
-            </form>
-          </div>
+              <div
+                id="formCoffe"
+                className="flex flex-col gap-y-2 placeholder-base-600 placeholder:text-sm"
+              >
+                <div className="flex flex-col gap-1">
+                  <input
+                    type="text"
+                    placeholder="CEP"
+                    className={`w-[200px] ${errors.cep && 'border-red-500'}`}
+                    {...register('cep')}
+                  />
 
-          <div className="bg-base-200 mt-4 p-10">
-            <div className="mb-8">
-              <div className="flex items-center gap-2">
-                <CurrencyDollar color="#8047F8" size={22} />
-                <h4 className="text-base-800">Pagamento</h4>
+                  {errors.cep && (
+                    <div className="text-red-500 text-sm italic">
+                      {errors.cep?.message}
+                    </div>
+                  )}
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <input type="text" placeholder="Rua" {...register('rua')} />
+                  <div className="text-red-500 text-sm italic">
+                    {errors.rua?.message}
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-inputGrid gap-3">
+                  <div className="flex flex-col gap-1">
+                    <input
+                      type="text"
+                      placeholder="Número"
+                      {...register('numero')}
+                    />
+                    <div className="text-red-500 text-sm italic">
+                      {errors.numero?.message}
+                    </div>
+                  </div>
+
+                  <input
+                    type="text"
+                    placeholder="Complemento"
+                    {...register('complemento')}
+                  />
+                </div>
+                <div className="grid grid-cols-secondInputGrid gap-3">
+                  <div className="flex flex-col gap-1">
+                    <input
+                      type="text"
+                      placeholder="Bairro"
+                      {...register('bairro')}
+                    />
+                    {errors.bairro && (
+                      <div className="text-red-500 text-sm italic">
+                        {errors.bairro?.message}
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col gap-1">
+                    <input
+                      type="text"
+                      placeholder="Cidade"
+                      {...register('cidade')}
+                    />
+                    {errors.cep && (
+                      <div className="text-red-500 text-sm italic">
+                        {errors.cidade?.message}
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col gap-1">
+                    <input type="text" placeholder="UF" {...register('uf')} />
+                    {errors.cep && (
+                      <div className="text-red-500 text-sm italic">
+                        {errors.uf?.message}
+                      </div>
+                    )}
+                  </div>
+                </div>
               </div>
-              <p className="text-sm ml-[30px] text-base-700">
-                O pagamento é feito na entrega. Escolha a forma que deseja pagar
-              </p>
             </div>
 
-            <div className="flex items-center gap-3">
-              <PaymentButtons />
+            <div className="bg-base-200 mt-4 p-10">
+              <div className="mb-8">
+                <div className="flex items-center gap-2">
+                  <CurrencyDollar color="#8047F8" size={22} />
+                  <h4 className="text-base-800">Pagamento</h4>
+                </div>
+                <p className="text-sm ml-[30px] text-base-700">
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+
+              <div className="flex flex-col gap-3">
+                <PaymentButtons />
+                {errors.paymentMethod && (
+                  <div className="text-red-500 text-sm italic">
+                    Defina seu método de pagamento
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div>
-        <h2 className="font-bold text-lg">Cafés selecionados</h2>
-        <Cart />
-      </div>
-    </div>
+        <SelectedCoffes />
+      </form>
+    </FormProvider>
   )
 }
--- src/components/PaymentButtons.tsx
+++ src/components/PaymentButtons.tsx
@@ -1,11 +1,29 @@
 import { Bank, CreditCard, Money } from '@phosphor-icons/react'
+import { useState } from 'react'
 import { useFormContext } from 'react-hook-form'
 
 export function PaymentButtons() {
   const {
     register,
+    watch,
     formState: { errors }
   } = useFormContext()
+
+  const [isCheckedInput, setIsCheckedInput] = useState('')
+
+  function handleInputCheckedCash() {
+    setIsCheckedInput('cash')
+  }
+
+  function handleInputCheckedDebit() {
+    setIsCheckedInput('debit')
+  }
+
+  function handleInputCheckedCredit() {
+    setIsCheckedInput('credit')
+  }
+
+  //I know about this dont be the better form to do that, i will refactor this.
 
   return (
     <div className="flex flex-col gap-2">
@@ -14,13 +32,17 @@ export function PaymentButtons() {
           <input
             type="radio"
             value="credit"
-            id="credit"
+            id="creditMethod"
+            className="hidden"
             {...register('paymentMethod')}
-            className="hidden checked:bg-purple-100 border-[1px] border-purple-300"
+            onClick={handleInputCheckedCredit}
           />
           <label
-            htmlFor="credit"
-            className="w-[190px] h-[51px] flex items-center bg-base-400 cursor-pointer text-xs uppercase gap-3 py-2 px-4 rounded-md transition-colors hover:bg-base-500 checked:text-red-800"
+            htmlFor="creditMethod"
+            className={`w-[180px] h-[51px] flex items-center bg-base-400 cursor-pointer text-xs uppercase gap-3 py-2 px-4 rounded-md transition-colors hover:bg-base-500 ${
+              isCheckedInput === 'credit' &&
+              'bg-purple-100 border-[1px] border-purple-300'
+            }`}
           >
             <CreditCard color="#8047F8" size={16} />
             Cartão de Crédito
@@ -31,13 +53,17 @@ export function PaymentButtons() {
           <input
             type="radio"
             value="debit"
-            id="debit"
+            id="debitMethod"
+            className="hidden"
             {...register('paymentMethod')}
-            className="hidden checked:bg-purple-100 border-[1px] border-purple-300"
+            onClick={handleInputCheckedDebit}
           />
           <label
-            htmlFor="debit"
-            className="w-[190px] h-[51px] flex items-center bg-base-400 cursor-pointer text-xs uppercase gap-3 py-2 px-4 rounded-md transition-colors hover:bg-base-500 checked:text-red-800"
+            htmlFor="debitMethod"
+            className={`w-[180px] h-[51px] flex items-center bg-base-400 cursor-pointer text-xs uppercase gap-3 py-2 px-4 rounded-md transition-colors hover:bg-base-500 ${
+              isCheckedInput === 'debit' &&
+              'bg-purple-100 border-[1px] border-purple-300'
+            }`}
           >
             <Bank color="#8047F8" size={16} />
             Cartão de Débito
@@ -48,18 +74,24 @@ export function PaymentButtons() {
           <input
             type="radio"
             value="cash"
-            id="cash"
+            id="cashMethod"
+            className="hidden"
             {...register('paymentMethod')}
-            className="hidden checked:bg-black"
+            onClick={handleInputCheckedCash}
           />
           <label
-            htmlFor="cash"
-            className="w-[190px] h-[51px] flex items-center bg-base-400 cursor-pointer text-xs uppercase gap-3 py-2 px-4 rounded-md transition-colors hover:bg-base-500 checked:text-red-800"
+            htmlFor="cashMethod"
+            className={`w-[180px] h-[51px] flex items-center bg-base-400 cursor-pointer text-xs uppercase gap-3 py-2 px-4 rounded-md transition-colors hover:bg-base-500 ${
+              isCheckedInput === 'cash' &&
+              'bg-purple-100 border-[1px] border-purple-300'
+            }`}
           >
             <Money color="#8047F8" size={16} />
             Dinheiro
           </label>
         </div>
+
+        
       </div>
     </div>
   )
